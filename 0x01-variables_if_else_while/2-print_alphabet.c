01ab09511d5332d3812e7138b5f1ad0233e6e2c8
Tonny Mwambingu <tonny.mwambingu@gmail.com>
2022-03-11 22:01:08 +0300

    Printing the alphabets of doom

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alpha[26] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+
+	for (i = 0; i < 26; i++)
+	{
+		putchar(alpha[i]);
+	}
+	putchar('\n');
+	return (0);
+
+}
+
