3edb210f827610273942775fd34e6e1f1e2a19be
Tonny Mwambingu <tonny.mwambingu@gmail.com>
2022-03-14 16:35:23 +0300

    Fixing Quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,14 +7,6 @@
  */
 int main(void)
 {
-	int i;
-	char quote[60] = "and that piece of art is useful\" \
-- Dora Korpar, 2015-10-19";
-
-	for (i = 0; i < 60; i++)
-	{
-		putchar(quote[i]);
-	}
-	putchar('\n');
+	write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 58);
 	return (1);
 }
