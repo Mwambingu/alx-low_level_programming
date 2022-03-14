b0c12073a38cd165bfbf3081c2f47ad48b04d344
Tonny Mwambingu <tonny.mwambingu@gmail.com>
2022-03-14 15:57:59 +0300

    Putting chars like Putin

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 1 (Success)
+ */
+int main(void)
+{
+	int i;
+	char quote[60] = "and that piece of art is useful\" \
+- Dora Korpar, 2015-10-19";
+
+	for (i = 0; i < 60; i++)
+	{
+		putchar(quote[i]);
+	}
+	putchar('\n');
+	return (1);
+}
