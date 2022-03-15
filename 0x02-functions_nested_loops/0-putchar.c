c867dd16b8d7d99955b7d6dfafda1ecc4db09600
Tonny Mwambingu <tonny.mwambingu@gmail.com>
2022-03-15 16:04:47 +0300

    Problems

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,14 +7,13 @@
  */
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
+	int i;
+	char word[] = "_putchar;
+
+	for (i = 0; i < 9; i++)
+	{
+	_putchar(word[i]);
+	}
 	_putchar('\n');
 	return (0);
 }
