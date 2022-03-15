275745341775652d3cdc61a826543203c2dfeedb
Tonny Mwambingu <tonny.mwambingu@gmail.com>
2022-03-15 18:10:28 +0300

    Is Alpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -7,17 +7,9 @@
  *
  * Return: 1 if character is lowercase, 0 otherwise.
  */
-int _islower(int c)
+int _isalpha(int c)
 {
 	if (isalpha(c))
-	{
-		return (1);
-	}
-	else if (isupper(c))
-	{
-		return (1);
-	}
-	else if (islower(c))
 	{
 		return (1);
 	}
