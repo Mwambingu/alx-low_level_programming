80e4064545ca7f0b52be8ec4d84d88ec7ef2ba68
Tonny Mwambingu <tonny.mwambingu@gmail.com>
2022-03-17 20:56:20 +0300

    Fixing Print most numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -5,14 +5,14 @@
  */
 void print_most_numbers(void)
 {
-int i;
+	int i;
 
-for (i = 48; i < 58; i++)
-{
-for (i = 48; i < 58; i++)
-{
-_putchar(i);
-}
-}
-_putchar('\n');
+	for (i = 48; i < 58; i++)
+	{
+		if (i != 50 && i != 52)
+		{
+			_putchar(i);
+		}
+	}
+	_putchar('\n');
 }
