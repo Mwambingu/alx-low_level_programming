6b3d5007a36f4e218b2c0010583c520ac5a9b872
Tonny Mwambingu <tonny.mwambingu@gmail.com>
2022-03-17 19:36:27 +0300

    Printing Most Numbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - Prints the numbers from 0-9 except for 2 and 4.
+ */
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		for (i = 48; i < 58; i++)
+		{
+			_putchar(i);
+		}
+	}
+	_putchar('\n');
+	return (0);
+}
