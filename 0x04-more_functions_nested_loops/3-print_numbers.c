4acdf1e8c939900c63c919f4057801802f38d0c7
Tonny Mwambingu <tonny.mwambingu@gmail.com>
2022-03-17 19:13:36 +0300

    Printing numbers 0 - 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_numbers - Prints the numbers from 0-9.
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		_putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
