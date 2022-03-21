bbaef98f2924dd38fc770de9f8cb32ce2bb88019
Tonny Mwambingu <tonny.mwambingu@gmail.com>
2022-03-21 21:12:08 +0300

    Fixing betty

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -2,11 +2,11 @@
 #include "main.h"
 /**
  * _strlen - Returns the length of a string.
- * @str: The string to get the length of.
+ * @s: The string to get the length of.
  *
  * Return: The length of @str.
  */
-int _strlen(char *s) 
+int _strlen(char *s)
 {
 	return (strlen(s));
 }
