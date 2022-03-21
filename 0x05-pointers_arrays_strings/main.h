c7d82bc02a370cd5ed1a80cf609eb2731a3a8176
Tonny Mwambingu <tonny.mwambingu@gmail.com>
2022-03-21 19:51:46 +0300

    Forgot my main

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+/**
+ * File: main.h
+ * Desc: Header file containing prototypes for all functions
+ *       used in the 0x04-pointers_arrays_strings directory.
+ */
+
+#include <stdlib.h>
+
+void reset_to_98(int *n);
+
+#endif
