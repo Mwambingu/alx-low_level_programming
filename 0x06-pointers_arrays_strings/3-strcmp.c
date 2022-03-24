70bf5a36d1a1cd797ee658ee3a32fa79e1857d52
Tonny Mwambingu <tonny.mwambingu@gmail.com>
2022-03-24 20:35:57 +0300

    Comparing Strings

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+/**
+ *_strcmp - compares two strings char by char
+ *          if strings are equal returns 0
+ * @s1: The first string
+ * @s2: The second string
+ *
+ * Return: returns value of _strcmp
+ */
+int _strcmp(char *s1, char *s2)
+{
+	return (strcmp(s1, s2));
+}
