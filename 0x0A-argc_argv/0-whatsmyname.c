fa7321bc8520590d1ab3184eeb8238dba4cfd11d
Tonny Mwambingu <tonny.mwambingu@gmail.com>
2022-04-01 20:36:12 +0300

    Printing program names

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ *@argc: argument counter
+ *@argv: argument vectors
+ *
+ *Return: returns 0 when successful
+ */
+int main(int argc, char **argv)
+{
+	printf("%s\n", *argv);
+	return (0);
+}
