f6ee88dc80d0cd8075d5a82debd5140add158092
Tonny Mwambingu <tonny.mwambingu@gmail.com>
2022-04-05 20:32:48 +0300

    printing grids

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,16 +1,46 @@
 #include "main.h"
 #include <stdlib.h>
 /**
- * free_grid - Frees a 2-dimensional array of integers.
- * @grid: The 2-dimensional array of integers to be freed.
- * @height: The height of grid.
+ * alloc_grid - Returns a pointer to a 2-dimensional array of
+ *               integers with each element initalized to 0.
+ * @width: The width of the 2-dimensional array.
+ * @height: The height of the 2-dimensional array.
+ *
+ * Return: If width <= 0, height <= 0, or the function fails - NULL.
+ *         Otherwise - a pointer to the 2-dimensional array of integers.
  */
-void free_grid(int **grid, int height)
+int **alloc_grid(int width, int height)
 {
-	int index;
+	int **i2D;
+	int h_index, w_index;
 
-	for (index = 0; index < height; index++)
-		free(grid[index]);
+	if (width <= 0 || height <= 0)
+		return (NULL);
 
-	free(grid);
+	i2D = malloc(sizeof(int *) * height);
+
+	if (i2D == NULL)
+		return (NULL);
+
+	for (h_index = 0; h_index < height; h_index++)
+	{
+		i2D[h_index] = malloc(sizeof(int) * width);
+
+		if (i2D[h_index] == NULL)
+		{
+			for (; h_index >= 0; h_index--)
+				free(i2D[h_index]);
+
+			free(i2D);
+			return (NULL);
+		}
+	}
+
+	for (h_index = 0; h_index < height; h_index++)
+	{
+		for (w_index = 0; w_index < width; w_index++)
+			i2D[h_index][w_index] = 0;
+	}
+
+	return (i2D);
 }
