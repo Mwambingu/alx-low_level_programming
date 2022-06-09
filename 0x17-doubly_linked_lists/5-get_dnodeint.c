7094edf0844669ff6235bc8662016a2affb83dcd
Tonny Mwambingu <tonny.mwambingu@gmail.com>
2022-06-09 18:51:03 +0300

    Hot Fix

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -4,7 +4,7 @@
  * get_dnodeint_at_index - finds a specific node of a linked list
  * @head: pointer to the beginning of the list
  * @index: index of the node to retrieve
- * 
+ *
  * Return: pointer to the indexed node, or NULL on failure
  */
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
